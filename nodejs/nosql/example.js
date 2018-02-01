5d9c0dbc093889466b5a14d1f110a5b83b6934d3
Marcelo Ribeiro de Sousa Lima <marcelo@trt3.jus.br>
2018-02-01 14:42:15 -0200

    Alteracao nos projetos exemplos para mqtt e sqlite

--- nodejs/nosql/example.js
+++ nodejs/nosql/example.js
@@ -1,12 +1,24 @@
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var sqlite = require('sqlite-sync')
+
+console.log('Script que testa mosquitto, sqlite-sync');
 
 client.on('offline', function () {
   console.log('Not able to connect\n')
   client.end()
 })
 
+sqlite.connect('myDatabase.db')
+
+var id= sqlite.insert('COMPANYS', {ID:1, NAME: 'TRT3'});
+
 console.log('Obtendo registros: \n')
+//Assíncrono
+
+sqlite.runAsync("SELECT * FROM COMPANYS", function(rows) {
+  console.log(rows);
+});
 
 client.on('connect', function () {
   client.subscribe('presence')
