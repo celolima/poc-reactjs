0521e35c886a8de455d013ce3318d117472e247d
Marcelo Ribeiro de Sousa Lima <marcelo@trt3.jus.br>
2018-02-01 15:02:20 -0200

    Adiciona exemplo de find no slite

--- nodejs/nosql/example.js
+++ nodejs/nosql/example.js
@@ -2,22 +2,26 @@ var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://test.mosquitto.org')
 var sqlite = require('sqlite-sync')
 
-console.log('Script que testa mosquitto, sqlite-sync');
+console.log('\nScript que testa mosquitto, sqlite-sync:\n');
 
 client.on('offline', function () {
-  console.log('Not able to connect\n')
+  console.log('\nNot able to connect on MQTT\n')
   client.end()
 })
 
 sqlite.connect('myDatabase.db')
 
-var id= sqlite.insert('COMPANYS', {ID:1, NAME: 'TRT3'});
+var id = sqlite.insert('COMPANYS', {ID:1, NAME: 'TRT3'});
+var id = sqlite.insert('COMPANYS', {ID:2, NAME: 'TRT2'});
 
-console.log('Obtendo registros: \n')
+console.log('Obtendo registros: ')
+
 //Assíncrono
-
 sqlite.runAsync("SELECT * FROM COMPANYS", function(rows) {
-  console.log(rows);
+    for( var key in rows ) {
+        console.log('Id  : %d', rows[key].ID)
+        console.log('Nome: %s', rows[key].NAME)
+    }
 });
 
 client.on('connect', function () {
